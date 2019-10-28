50e98750f6ee51e18f694d7d0ed25f3b91aca55b
yarosha <yarosha_ponomar@mail.ru>
2019-10-28 10:04:23 +0300

    someone commit

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,1 +1,7 @@
-class Cinemaparser
+class CinemaParser():
+    def __init__(self, city="msk"):
+        self.city = city
+        
+    def extract__raw_content():
+        self.content = requests.get(url = 'https://' + self.city + '.subscity.ru'
+        
